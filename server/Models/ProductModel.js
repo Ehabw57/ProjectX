2d52c3d723b832351d3926e7f8d699568cf64069
Ehabw57 <maestro.ehab123@gmail.com>
2025-10-30 11:24:09 +0300

    Create product model

---
+++ server/Models/ProductModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    imageUrl:{
+        type: String,
+        required: true
+    },
+    categoryId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+});
+
+module.exports = mongoose.model('Product', productSchema);
