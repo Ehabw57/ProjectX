e2ae06b46468622ceb55fbb1d13a8f7f84efade5
Ehabw57 <maestro.ehab123@gmail.com>
2025-10-30 11:46:59 +0300

    Create server entryPoint

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ProductRouter = require("./Routes/ProductRoute");
+
+const PORT = process.env.PORT || 3000;
+const DBURL = process.env.DBURL || "mongodb://localhost:27017/testDb";
+const app = express();
+
+mongoose
+  .connect(DBURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use("/products", ProductRouter);
+
+app.get("/", (req, res) => {
+  res.send("Yo I'm Good And Running!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
