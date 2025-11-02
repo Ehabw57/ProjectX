3f4e6bb085e236681c1717c3fd17e9bff6e3a261
Ehabw57 <maestro.ehab123@gmail.com>
2025-11-02 12:39:10 +0200

    Create intial client side

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import Table from "./components/Table";
+function App() {
+  return (
+    <>
+  <h1 class="bg-amber-300 text-xl">Hello Vite + React! </h1>
+  <Table/>
+  </>
+  )
+}
+
+export default App;
---
+++ client/src/components/Table.jsx
@@ -0,0 +1,3 @@
+export default function Table({ data }) {
+  return <h1>Table Component</h1>;
+}
