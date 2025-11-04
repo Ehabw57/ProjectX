d89361c79beecb0ecc61f56ffa1ee609e1a973c1
Ehabw57 <maestro.ehab123@gmail.com>
2025-11-04 17:23:26 +0200

    Update app entry point

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ app.engine(
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(cors());
 app.use("/product", ProductRouter);
